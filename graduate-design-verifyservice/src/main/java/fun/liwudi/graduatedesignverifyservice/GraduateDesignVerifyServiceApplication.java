a2937691e94c26c157988b6bd87b761d34930f05
Liwu-di <liwudi@liwudi.fun>
2021-04-11 22:52:12 +0800

    redis

--- graduate-design-verifyservice/src/main/java/fun/liwudi/graduatedesignverifyservice/GraduateDesignVerifyServiceApplication.java
+++ graduate-design-verifyservice/src/main/java/fun/liwudi/graduatedesignverifyservice/GraduateDesignVerifyServiceApplication.java
@@ -3,10 +3,22 @@ package fun.liwudi.graduatedesignverifyservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
 
 /**
  * @author 李武第
@@ -18,6 +30,15 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @MapperScan("fun.liwudi.graduatedesignverifyservice.mapper")
 @EnableZuulProxy
 public class GraduateDesignVerifyServiceApplication {
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GraduateDesignVerifyServiceApplication.class, args);
---
+++ graduate-design-verifyservice/src/main/java/fun/liwudi/graduatedesignverifyservice/rest/BiCodeVerifyRest.java
@@ -0,0 +1,12 @@
+package fun.liwudi.graduatedesignverifyservice.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 李武第
+ */
+@RestController
+public class BiCodeVerifyRest {
+
+
+}
