091ba94816d5596f1052f93d61886a08728cbd3a
Liwu-di <liwudi@liwudi.fun>
2021-08-12 15:25:48 +0800

    配置中心微服务改造

--- spring-cloud-config-prod/src/main/java/fun/liwudi/springcloudconfig/SpringCloudConfigApplication.java
+++ spring-cloud-config-prod/src/main/java/fun/liwudi/springcloudconfig/SpringCloudConfigApplication.java
@@ -2,6 +2,7 @@ package fun.liwudi.springcloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringCloudConfigApplication {
 
     public static void main(String[] args) {
