cc13d29e62d099c6c421d69c9b524dc7d987e67a
Liwu-di <liwudi@liwudi.fun>
2021-08-17 00:51:27 +0800

    get,post,put,delete 请求外部url,带参数

--- common-part/src/main/java/fun/liwudi/commonpart/helper/UrlAccessHelper.java
+++ common-part/src/main/java/fun/liwudi/commonpart/helper/UrlAccessHelper.java
@@ -1,11 +1,16 @@
 package fun.liwudi.commonpart.helper;
 
+import com.alibaba.fastjson.JSONObject;
 import fun.liwudi.domain.bean.AccessParameter;
 import fun.liwudi.domain.constants.Constant;
 import fun.liwudi.domain.dto.JsonResponse;
 import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,17 +47,68 @@ public class UrlAccessHelper {
                 }
             }
             uri = uriBuilder.build();
+            if(Objects.isNull(uri)){
+                return jsonResponseHelper.getJsonResponse(Constant.CANNOT_ACCESS_URL,Constant.ERROR);
+            }
+            if(!CollectionUtils.isEmpty(accessParameter.getHeaderParameter())){
+                HttpHeaders headers = new HttpHeaders();
+                for(Map.Entry<String,String> kv : accessParameter.getHeaderParameter().entrySet()){
+                    headers.add(kv.getKey(),kv.getValue());
+                }
+                ResponseEntity<JsonResponse> response = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity(headers), JsonResponse.class);
+                return response.getBody();
+            }
+            else {
+                ResponseEntity<JsonResponse> responseEntity = restTemplate.getForEntity(uri, JsonResponse.class);
+                return responseEntity.getBody();
+            }
+
         } catch (URISyntaxException e) {
             logger.error(e.getMessage(),e);
-        }
-
-        if(Objects.nonNull(uri)){
-            return restTemplate.getForObject(uri,JsonResponse.class);
-        }
-        else {
             return jsonResponseHelper.getJsonResponse(Constant.CANNOT_ACCESS_URL,Constant.ERROR);
         }
     }
 
+    public JsonResponse accessByUrlUsePost(@RequestBody AccessParameter accessParameter){
+        try {
+            if(!CollectionUtils.isEmpty(accessParameter.getHeaderParameter())){
+                HttpHeaders headers = new HttpHeaders();
+                for(Map.Entry<String,String> kv : accessParameter.getHeaderParameter().entrySet()){
+                    headers.add(kv.getKey(),kv.getValue());
+                }
+                ResponseEntity<JsonResponse> response = restTemplate.exchange(accessParameter.getUrl(), HttpMethod.POST, new HttpEntity(accessParameter.getBodyParameter(),headers), JsonResponse.class);
+                return response.getBody();
+            }
+            else {
+                JsonResponse jsonResponse = restTemplate.postForObject(accessParameter.getUrl(),accessParameter.getBodyParameter(), JsonResponse.class);
+                return jsonResponse;
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(),e);
+            return jsonResponseHelper.getJsonResponse(Constant.CANNOT_ACCESS_URL,Constant.ERROR);
+        }
+    }
+
+    public JsonResponse accessByUrlUsePut(@RequestBody AccessParameter accessParameter){
+        HttpHeaders headers = new HttpHeaders();
+        if(!CollectionUtils.isEmpty(accessParameter.getHeaderParameter())){
+            for(Map.Entry<String,String> kv : accessParameter.getHeaderParameter().entrySet()){
+                headers.add(kv.getKey(),kv.getValue());
+            }
+        }
+        ResponseEntity<JsonResponse> response = restTemplate.exchange(accessParameter.getUrl(), HttpMethod.PUT, new HttpEntity(accessParameter.getBodyParameter(), headers), JsonResponse.class);
+        return response.getBody();
+    }
+
+    public JsonResponse accessByUrlUseDelete(@RequestBody AccessParameter accessParameter){
+        HttpHeaders headers = new HttpHeaders();
+        if(!CollectionUtils.isEmpty(accessParameter.getHeaderParameter())){
+            for(Map.Entry<String,String> kv : accessParameter.getHeaderParameter().entrySet()){
+                headers.add(kv.getKey(),kv.getValue());
+            }
+        }
+        ResponseEntity<JsonResponse> response = restTemplate.exchange(accessParameter.getUrl(), HttpMethod.DELETE, new HttpEntity(accessParameter.getBodyParameter(), headers), JsonResponse.class);
+        return response.getBody();
+    }
 
 }
--- domain/src/main/java/fun/liwudi/domain/bean/AccessParameter.java
+++ domain/src/main/java/fun/liwudi/domain/bean/AccessParameter.java
@@ -7,9 +7,22 @@ import java.util.Map;
  */
 public class AccessParameter {
 
+    /**
+     * 路径参数直接在url中构造好
+     */
     private String url;
 
+    /**
+     * 这里默认是请求头（get），请求体参数（POST）
+     */
     private Map<String,String> parameter;
+
+    /**
+     * header参数，一般用来鉴权
+     */
+    private Map<String,String> headerParameter;
+
+    private Object bodyParameter;
 
     public String getUrl() {
         return url;
@@ -27,11 +40,29 @@ public class AccessParameter {
         this.parameter = parameter;
     }
 
+    public Map<String, String> getHeaderParameter() {
+        return headerParameter;
+    }
+
+    public void setHeaderParameter(Map<String, String> headerParameter) {
+        this.headerParameter = headerParameter;
+    }
+
+    public Object getBodyParameter() {
+        return bodyParameter;
+    }
+
+    public void setBodyParameter(Object bodyParameter) {
+        this.bodyParameter = bodyParameter;
+    }
+
     @Override
     public String toString() {
         return "AccessParameter{" +
                 "url='" + url + '\'' +
                 ", parameter=" + parameter +
+                ", headerParameter=" + headerParameter +
+                ", bodyParameter=" + bodyParameter +
                 '}';
     }
 }
